46d0aee4c5f2d9b7802710957c2b1d035b9eb67b
OmerAyg <omeraygun438@gmail.com>
2024-07-08 12:35:29 +0300

    Add SQLite packages and initial setup configuration

--- obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7088070a402d6a3177a95159c7d3e03dd72fd651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96284ccb1c097d1e944a31a32eb12167cb29790")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
